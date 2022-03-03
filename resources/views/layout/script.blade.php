03fcf0e86f8894ffcb4c04691a26800d0992eb96
ArvaBayu <arvabayususanto@gmail.com>
2022-03-03 21:26:30 +0700

    first commit

---
+++ resources/views/layout/script.blade.php
@@ -0,0 +1,20 @@
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="asset/dailyShop/js/bootstrap.js"></script>  
+<!-- SmartMenus jQuery plugin -->
+<script type="text/javascript" src="asset/dailyShop/js/jquery.smartmenus.js"></script>
+<!-- SmartMenus jQuery Bootstrap Addon -->
+<script type="text/javascript" src="asset/dailyShop/js/jquery.smartmenus.bootstrap.js"></script>  
+<!-- To Slider JS -->
+<script src="asset/dailyShop/js/sequence.js"></script>
+<script src="asset/dailyShop/js/sequence-theme.modern-slide-in.js"></script>  
+<!-- Product view slider -->
+<script type="text/javascript" src="asset/dailyShop/js/jquery.simpleGallery.js"></script>
+<script type="text/javascript" src="asset/dailyShop/js/jquery.simpleLens.js"></script>
+<!-- slick slider -->
+<script type="text/javascript" src="asset/dailyShop/js/slick.js"></script>
+<!-- Price picker slider -->
+<script type="text/javascript" src="asset/dailyShop/js/nouislider.js"></script>
+<!-- Custom js -->
+<script src="asset/dailyShop/js/custom.js"></script> 
